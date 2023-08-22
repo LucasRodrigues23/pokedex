be89883af9b7c20b193abca8efa04e2d8a2adb6c
Lucas Rodrigues <lcs.rs23@gmail.com>
2023-08-22 12:12:12 -0300

    feat initial crud

---
+++ src/components/header/index.tsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/pokemon.svg'
+import styles from './styles.module.scss'
+
+export default function Header() {
+    return (
+        <header className={styles.container}>
+            <Image />
+        </header>
+    )
+}
